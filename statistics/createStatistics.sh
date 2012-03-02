c2b68963a9d201f8a2abf76b8a43621c6f08f3a4
Pasieka Manuel <mapa17@posgrado.upv.es>
2012-03-02 16:25:47 +0000

    Removing swap files

--- statistics/createStatistics.sh
+++ statistics/createStatistics.sh
@@ -5,20 +5,21 @@ then
 	echo "No argument given, assuming script is called to generate images!"
 
 	#Cleanup
-	rm *.png *.Rout *.pdf
+	rm *.png *.Rout *.pdf 2>/dev/null
 
 	R CMD BATCH downloadTime.R
 	R CMD BATCH Conn.R
 	R CMD BATCH peerCnt.R
 
-	convert *.png stats.pdf
-	rm *.Rout
-	rm Rplots.pdf
+	convert *.png Statistic_Summary.pdf
+	rm *.Rout Rplots.pdf 2>/dev/null
+
+	echo "Created pdf Statistic_Summary.pdf"
 
 elif [ "$1" == "clear" ]
 then
 	echo "Clearing directory!"
-	rm *.png *.Rout *.pdf
+	rm *.png *.Rout *.pdf 2>/dev/null
 fi
 exit
 
